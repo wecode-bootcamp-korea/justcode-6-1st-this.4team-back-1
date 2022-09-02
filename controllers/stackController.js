9f71c5bd6d68a7e77e91e32eb980f0e13f991c2f
hyunjung <hhhj.1486@gmail.com>
2022-09-02 09:45:59 +0900

    사용자관련 기능 수정

--- controllers/stackController.js
+++ controllers/stackController.js
@@ -1,11 +1,16 @@
 const stackService = require('../services/stackService');
 
+// 기술 스택 목록 가져오기.
 const getStackList = async (req, res) => {
   const { classification } = req.body;
 
-  const stacks = await stackService.getStackList(classification);
-
-  return res.status(200).json({ stacks });
+  try {
+    const stacks = await stackService.getStackList(classification);
+    return res.status(200).json({ stacks });
+  } catch (err){
+    res.status(err.status || 500).json(err.message);
+  }
+  
 }
 
 module.exports = { getStackList }
